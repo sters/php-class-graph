d132f2d3e6fd7216e93bf2d67a1dd2a3f9b56889
sters <sters@users.noreply.github.com>
2023-03-23 00:34:08 +0900

    fix

--- src/Traverser.php
+++ src/Traverser.php
@@ -2,167 +2,51 @@
 
 namespace ClassGraph;
 
-use PhpParser\Node;
-use PhpParser\Node\Expr;
-use PhpParser\Node\Stmt;
-use PhpParser\NodeVisitor;
-use PhpParser\NodeVisitorAbstract;
+use PhpParser\Error;
+use PhpParser\NodeTraverser;
+use PhpParser\Parser;
+use PhpParser\ParserFactory;
 
 /**
- * Visitor is implemented NodeVisitor for dependency finding
+ * Traverser is wrapper of PhpParser->parse and PhpParser->traverse
  */
-class Visitor extends NodeVisitorAbstract implements NodeVisitor
+class Traverser
 {
-    protected $namespace = '';
-    protected $class = '';
-    protected $uses = [];
-    protected $skip = [
-        'self', 'static', 'parent',
-    ];
+    /** @var Parser */
+    protected $parser;
 
     /** @var \Closure[] function(Node $node) */
     protected $hooks = [];
 
-    public function enterNode(Node $node)
+    public function __construct()
     {
-        // namespace Foo;
-        if ($node instanceof Stmt\Namespace_ && $this->namespace === '' && !empty($node->name->parts)) {
-            $this->namespace = implode('\\', $node->name->parts);
-        }
-
-        // class Foo extends Bar implements Hoge; => Bar, Hoge
-        if ($node instanceof Stmt\Class_ && $this->class === '') {
-            $this->class = $node->name;
-            if (!is_string($node->name)) {
-                $this->class = $node->name->name;
-            }
-
-            // class Foo extends Bar; => Bar
-            if (!is_null($node->extends)) {
-                $this->addUsesForNameParts($node->extends->parts);
-            }
-            // class Foo implements Hoge; => Hoge
-            if (!empty($node->implements)) {
-                $this->addUsesForNameParts($node->implements);
-            }
-        }
-
-        // use Foo\Bar as Hoge; => Foo\Bar
-        if ($node instanceof Stmt\UseUse) {
-            if (!empty($node->alias)) {
-                $this->skip[] = $node->alias->name;
-            }
-            $this->addUsesForNameParts($node->name, true);
-            $this->skip[] = implode('\\', $node->name->parts);
-            $this->skip[] = $node->name->parts[count($node->name->parts) - 1];
-            $this->skip = array_unique($this->skip);
-        }
-
-        // Foo\Bar::HOGE => Foo\Bar
-        if ($node instanceof Expr\ClassConstFetch) {
-            $this->addUsesForNameParts($node->class);
-        }
-
-        // Foo\Bar::id => Foo\Bar
-        if ($node instanceof Expr\StaticPropertyFetch) {
-            $this->addUsesForNameParts($node->class);
-        }
-
-        // Foo\Bar::get(); => Foo\Bar
-        if ($node instanceof Expr\StaticCall) {
-            $this->addUsesForNameParts($node->class);
-        }
-
-        // new Foo(); => Foo
-        if ($node instanceof Expr\New_) {
-            $this->addUsesForNameParts($node->class);
-        }
-
-        foreach ($this->hooks as $hook) {
-            $hook($node);
-        }
-    }
-
-    private function addUsesForNameParts($parts, $raw = false)
-    {
-        if (empty($parts)) {
-            return;
-        }
-
-        if (is_object($parts)) {
-            if (!empty($parts->parts)) {
-                $this->addUsesForNameParts($parts->parts, $raw);
-            }
-            return;
-        }
-
-        $className = $parts;
-        if (is_array($parts)) {
-            $className = implode('\\', $parts);
-        }
-        if ($className[0] === '\\' || $raw) {
-            $this->uses[] = $className;
-            $this->uses = array_unique($this->uses);
-            return;
-        }
-        if (in_array($className, $this->skip)) {
-            return;
-        }
-
-        $name = '';
-        if (!empty($this->namespace) && !$raw) {
-            $found = false;
-            foreach ($this->skip as $skip) {
-                if ($skip === $className) {
-                    $found = true;
-                    break;
-                }
-            }
-            if (!$found) {
-                $name .= $this->namespace . '\\';
-            }
-        }
-
-        $this->uses[] = $name . $className;
-        $this->uses = array_unique($this->uses);
+        $this->parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
     }
 
     /**
-     * @return string|null
+     * do traverse
+     *
+     * @param string $sourceFile
+     * @return Visitor visited file information
      */
-    public function getClass(): ?string
+    public function traverse(string $sourceFile): Visitor
     {
-        return $this->class;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getNamespace(): ?string
-    {
-        return $this->namespace;
-    }
-
-    /**
-     * @return array
-     */
-    public function getUses(): array
-    {
-        return $this->uses;
-    }
-
-    /**
-     * @return string
-     */
-    public function getFullClassName(): string
-    {
-        $ns = $this->getNamespace();
-        if (!empty($ns)) {
-            $ns = $ns . '\\';
-        } else {
-            $ns = '';
+        $visitor = new Visitor;
+        foreach($this->hooks as $hook) {
+            $visitor->addHook($hook);
         }
-        return $ns . $this->getClass();
+
+        try {
+            $ast = $this->parser->parse(file_get_contents($sourceFile));
+        } catch (Error $error) {
+            return $visitor;
+        }
+
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor($visitor);
+        $traverser->traverse($ast);
+
+        return $visitor;
     }
 
     /**
@@ -170,6 +54,6 @@ class Visitor extends NodeVisitorAbstract implements NodeVisitor
      */
     public function addHook(\Closure $f)
     {
-        $this->hooks[] = \Closure::bind($f, $this, get_class());
+        $this->hooks[] = $f;
     }
 }
